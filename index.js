424245738356c50d75e23b930d99748481ee23c2
enoskyeza <enoskyeza@gmail.com>
2023-11-20 22:23:33 +0300

    Add challenge 2

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { createRoot } from 'react-dom/client'
+
+
+
+// Render your React component instead
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, world</h1>);
+
+
+
+
+
 /* Challenge:
 
 Create a navbar in JSX:
@@ -9,15 +23,19 @@ Create a navbar in JSX:
     - Don't worry about styling yet - it'll just be plain-looking HTML for now
 
  */
+
 const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
+    <div>
+        <img src='./react-logo.png' width={ 40 }></img>
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released inn 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k starts on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
 
 ReactDOM.render(navbar, document.getElementById("root"))

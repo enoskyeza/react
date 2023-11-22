874b03416b94da6d42d4ec9b68c714651fe5d2a8
enoskyeza <enoskyeza@gmail.com>
2023-11-22 23:08:10 +0300

    Add about info

--- vite-project/src/components/MainContent.jsx
+++ vite-project/src/components/MainContent.jsx
@@ -30,6 +30,12 @@ const Main = () => {
                         <button className='dc-btn dc-btn-blue'>LinkedIn</button>
                     </div>
                 </div>
+                <div className='about-info'>
+                    <h4>About</h4>
+                    <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and amm always looking for new things to learn.</p>
+                    <h4>Interests</h4>
+                    <p>Food expert. Music. Reader. Internnet fanatic. Bacon buff. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+                </div>
             </div>
             <div className='digital-card-footer'></div>
         </div>

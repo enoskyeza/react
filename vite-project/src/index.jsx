d6461cc278462e66f5f3664fa288e0bac9bebf28
enoskyeza <enoskyeza@gmail.com>
2023-11-22 04:45:56 +0300

    Rename js files to jsx

--- vite-project/src/index.jsx
+++ vite-project/src/index.jsx
@@ -1,23 +1,9 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Page from './App'
 
-import Header from '../../Header'
-import MainContent from '../../MainContent'
-import  Footer from '../../Footer'
-
-
-function Page() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
-ReactDOM.render(
-    <Page />,
-    document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render( <Page /> )
 
 
 
@@ -117,6 +103,29 @@ excited to be learning React :)
 Render your list to the page
 
  */
+
+
+/**
+Challenge: Project setup
+
+- Create a `components` folder
+- Create the following components in separate files inside
+  the components folder.  In each one, just render an `h1`
+  with the name of the component (e.g. return <h1>Navbar goes here</h1>):
+    - Navbar
+    - Main
+- Create an App component outside the components folder (sibling to
+  the index.js file)
+    - Have App render the Navbar and Main components
+- Import and render the App component inside of index.js using ReactDOM
+    - At this point you should have your "Navbar goes here" etc. showing up
+      in the mini-browser.
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the @import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this
+  works if you haven't done it before)
+*/
 
 
 
